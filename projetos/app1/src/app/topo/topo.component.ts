9be44fc678355f353588df7be60d18b3203d652c
vbpfelipe <felipe.vbpinheiro@gmail.com>
2021-09-02 08:18:30 -0300

    Projeto exemplo em Angular 4

---
+++ projetos/app1/src/app/topo/topo.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-topo',
+    templateUrl: './topo.component.html',
+    /*styles: [` .exemplo { 
+                    color: red
+                } `
+            ]*/
+    styleUrls: ['./topo.component.css' //, 
+                // './topo2.component.css'
+                ]
+})
+export class TopoComponent {
+
+}
